981d08920680869a353f5f5b3cdf299abfbe82bc
Seil Cho <47363098+seilcho7@users.noreply.github.com>
2019-05-16 08:20:12 -0400

    add build

---
+++ build/precache-manifest.496dcbc8d15e7aba615654fd24a38aa5.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619400750987543fe9f0667c0c777d35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54f7982fc7b5591e804e",
+    "url": "/static/css/main.dbe31f9a.chunk.css"
+  },
+  {
+    "revision": "6e5f1687a4d36f2047eb",
+    "url": "/static/js/2.6a9e0da5.chunk.js"
+  },
+  {
+    "revision": "54f7982fc7b5591e804e",
+    "url": "/static/js/main.c6028442.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ace39cabf849cc9c2802d0f0d18cd97e",
+    "url": "/static/media/allyrion.ace39cab.jpg"
+  },
+  {
+    "revision": "2c296d2d49dd7fddef1487221404ba46",
+    "url": "/static/media/arryn.2c296d2d.jpg"
+  },
+  {
+    "revision": "751198473309651dc085108f37b2b0dd",
+    "url": "/static/media/baratheon.75119847.jpg"
+  },
+  {
+    "revision": "d932428c84142483e040e9c69ab9f414",
+    "url": "/static/media/cerwyn.d932428c.jpg"
+  },
+  {
+    "revision": "768740685c531137b071c2ba1daedae5",
+    "url": "/static/media/clegane.76874068.jpg"
+  },
+  {
+    "revision": "2deca13686cbf373f264a44fe4fb1040",
+    "url": "/static/media/crakehall.2deca136.jpg"
+  },
+  {
+    "revision": "ee06d2abf2b7e49cb75974c168ddcd9e",
+    "url": "/static/media/dondarrion.ee06d2ab.jpg"
+  },
+  {
+    "revision": "db4c2fd18750d7dbd48e0f41233c80f3",
+    "url": "/static/media/frey.db4c2fd1.jpg"
+  },
+  {
+    "revision": "9951b249475c5da4e23e9335ad702b5a",
+    "url": "/static/media/greyjoy.9951b249.jpg"
+  },
+  {
+    "revision": "d9247d10a2b3fe28f00347fd15074098",
+    "url": "/static/media/hornwood.d9247d10.jpg"
+  },
+  {
+    "revision": "5acdab0f9415c2f306e6424c313c0976",
+    "url": "/static/media/karstark.5acdab0f.jpg"
+  },
+  {
+    "revision": "c317eb94bbbe7913c3fe6631d82a7a4e",
+    "url": "/static/media/lannister.c317eb94.jpg"
+  },
+  {
+    "revision": "ac8eb979011c6721ce7b2b8085630c56",
+    "url": "/static/media/logo.ac8eb979.png"
+  },
+  {
+    "revision": "d3ac5d496a9d22cad979c3beec90b736",
+    "url": "/static/media/mallister.d3ac5d49.jpg"
+  },
+  {
+    "revision": "25e7552f3896e6e18a41568c5773b150",
+    "url": "/static/media/martell.25e7552f.jpg"
+  },
+  {
+    "revision": "928072bc0924558b858c492ca8c75765",
+    "url": "/static/media/mormont.928072bc.jpg"
+  },
+  {
+    "revision": "9c28bbddbe29d144bcafe54ffa16a7ad",
+    "url": "/static/media/seaworth.9c28bbdd.jpg"
+  },
+  {
+    "revision": "a60c2caa83414f7ef71a87d835a46cc0",
+    "url": "/static/media/selmy.a60c2caa.jpg"
+  },
+  {
+    "revision": "03b5070ab2ca8708b562d8c665f526b7",
+    "url": "/static/media/stark.03b5070a.jpg"
+  },
+  {
+    "revision": "b2d6c045bc83fbac57492447ea3f4d3e",
+    "url": "/static/media/swyft.b2d6c045.jpg"
+  },
+  {
+    "revision": "ba4494812364cf70a08a48d76324e192",
+    "url": "/static/media/targaryen.ba449481.jpg"
+  },
+  {
+    "revision": "bb2303c6bd8680be5e31cdbee738d56c",
+    "url": "/static/media/tully.bb2303c6.jpg"
+  },
+  {
+    "revision": "97476e26a43a0af793b9263aa3fc3e46",
+    "url": "/static/media/tyrell.97476e26.jpg"
+  },
+  {
+    "revision": "8d9e4217d955ea3fe225000cf6b97a5a",
+    "url": "/static/media/westerling.8d9e4217.jpg"
+  }
+]);
